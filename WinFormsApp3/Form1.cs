133da3eeb6648254e0184f7624a794e7be7903a9
Trsh16 <tlikorawung@student.ciputra.ac.id>
2022-04-04 19:59:38 +0700

    1

--- WinFormsApp3/Form1.cs
+++ WinFormsApp3/Form1.cs
@@ -24,21 +24,23 @@ namespace WinFormsApp3
                             String str)
             {
                 int n = str.Length;
-                char[] hashChar = new char[26];
+                char[] Charset = new char[26];
                 for (int i = 0; i < 26; i++)
                 {
-                    int ch = Math.Abs(charSet[i] - 'a');
-                    hashChar[ch] = (char)('a' + i);
-                }
-               
-                String s = textBox_Menjadi.Text;
+                    if (Charset[i] == textBox_Kalimat.TextLength)
+                    {
+                        int ch = Math.Abs(charSet[i] - 'a');
+                        Charset[ch] = (char)('a' + i);
+                    }
+                }          
+                String s = textBox_Kalimat.Text;
                 for (int i = 0; i < n; i++)
                 {
-                    s += hashChar[str[i] - 'a'];
+                    s += Charset[str[i] - 'a'];
                 }
                 return s;
             }
-            char[] charSet = "cdefghijklmnopqrstuvwxyzab".ToCharArray();
+            char[] charSet = new char[26] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
             String str = textBox_Kalimat.Text;
             str = conversion(charSet, str);
             label_Hasil.Text = str.ToUpper();
